14c5bdeece55ec7eec5f4861e57cb28d13938b0b
Leonardo Bocos <LE23080289@merida.tecnm.mx>
2026-09-03 17:01:52 -0600

    First Commit

---
+++ src/main/java/com/tecnm/merida/market_backendjavaj/MarketBackendjavajApplication.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.market_backendjavaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackendjavajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBackendjavajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecnm/merida/market_backendjavaj/MarketBackendjavajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.market_backendjavaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackendjavajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
